d2edbc5c0ada16476f1657e861615d33783ec3e8
Odisseuss <rares_lungescu@yahoo.com>
2020-06-25 14:27:45 +0300

    intialize other components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Logos from './components/logos/logos';
 import './App.css';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Logos></Logos>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/createNote/CreateNote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class CreateNote extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return (
+			<div>
+				<form>
+					<textarea></textarea>
+					<button></button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default CreateNote;
