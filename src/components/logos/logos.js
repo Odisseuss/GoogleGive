b8f9c2508ac2cc6c995ac08b3a1da42a3948fbdd
Odisseuss <rares_lungescu@yahoo.com>
2020-06-25 14:26:26 +0300

    Create logos header

---
+++ src/components/logos/logos.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './logos.css';
+const logos = () => {
+	return (
+		<div className='logos'>
+			<img src='https://firebasestorage.googleapis.com/v0/b/give-a5ae5.appspot.com/o/firebase-logo.c24b6b9c.png?alt=media&token=91245c48-30b7-46a4-9261-760fd43e0fa1'></img>
+			<span className='material-icons add'>add</span>
+			<img src='https://firebasestorage.googleapis.com/v0/b/give-a5ae5.appspot.com/o/logo512.png?alt=media&token=77255dcb-a888-4f98-bb8c-7fd1e889935b'></img>
+			<div className='plus-icon'>
+				<span className='material-icons'>horizontal_rule</span>
+				<span className='material-icons'>horizontal_rule</span>
+			</div>
+			<img src='https://firebasestorage.googleapis.com/v0/b/give-a5ae5.appspot.com/o/keep-logo.579de524.png?alt=media&token=a15b7006-2957-4a49-8f6d-a26e837dd157'></img>
+		</div>
+	);
+};
+
+export default logos;
