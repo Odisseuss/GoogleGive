9c8b76f776544d67e0185d3830bc3a32f8bb8bd9
Rares <rares_lungescu@yahoo.com>
2020-09-03 20:47:29 +0300

    fix problems with cloudbuild

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Logos from "./components/logos/Logos";
+import Logos from "./components/logos/logos";
 import CreateNote from "./components/createNote/CreateNote";
 import Settings from "./components/settings/Settings";
 import Card from "./components/card/Card";
